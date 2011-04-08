fa12db2773b8b837fd2b6c6a7ee09b5b19e15d0b
rails user <stevek91411@yahoo.com>
2011-04-07 18:49:47 -0700

    next

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,8 @@ require 'rails/all'
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module R7
-  class Application < Rails::Application
+  class Application < Rails::Application  	
+  	
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
---
+++ newdb.bat
@@ -0,0 +1,2 @@
+call rake db:migrate 
+call rake db:fixtures:load
